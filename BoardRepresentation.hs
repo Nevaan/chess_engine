f70343e291545ec9b04a739ce04c1f0df783716f
Nevaan <pawel.losek@best.krakow.pl>
2015-06-03 23:34:37 +0200

    Corrected filename

---
+++ BoardRepresentation.hs
@@ -0,0 +1,19 @@
+module BoardRepresentation where
+
+data Colour = Black | White deriving Show
+data Figure = King | Queen | Rook | Bishop | Knight | Pawn deriving Show
+data ColouredFigure = ColouredFigure Colour Figure deriving Show
+
+type Square = Maybe ColouredFigure
+type Board = [[Square]]
+type Position = (Int, Int) -- starting with upper left corner, coordinates are (x,y)
+
+initBoard = unlines [ "RNBQKBNR",
+                      "PPPPPPPP",
+                      "        ",
+                      "        ",
+                      "        ",
+                      "        ",
+                      "pppppppp",
+                      "rnbqkbnr"
+                    ]
